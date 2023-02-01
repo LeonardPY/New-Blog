e9093de5dfb03318988ca170ecd1938f891b336f
Leo <leonardmartirosyan95@gmail.com>
2023-02-01 21:21:07 +0400

    Route Web change

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 
 //Route::get('phpinfo', fn () => phpinfo());
+Auth::routes(['verify' => 'true']);
 
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('main.index');
@@ -92,5 +84,3 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 
         Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
     });
 });
-
-Auth::routes(['verify' => 'true']);
